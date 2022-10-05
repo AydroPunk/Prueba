8d810f03c97547e59d33300b347a9e8d072878b8
aydropunk <AydroPunk@tutamail.com>
2022-10-05 16:14:34 -0500

    Cambios por take

--- Hola.java
+++ Hola.java
@@ -2,5 +2,7 @@ public class Hola {
     public static void main(String[] args) {
         //no hola
         System.out.println("Esto es una prueba");
+        System.out.println("HOLA DAVID, 2DA PRUEBA");
+        System.out.println("3era prueba");
     }
 }
